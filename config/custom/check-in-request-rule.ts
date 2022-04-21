e515865e1324cc939381af1d032af58b618af6c2
Dima Barbashov <tgskiv@gmail.com>
2022-04-21 19:49:47 +0300

    requestCheckInOrganizationChannel set to false

--- config/custom/check-in-request-rule.ts
+++ config/custom/check-in-request-rule.ts
@@ -1,3 +1,6 @@
 import type { Nullable, CheckInRequestRule } from '../../types';
 
-export const checkInRequestRule: Nullable<CheckInRequestRule> = null;
+export const checkInRequestRule: Nullable<CheckInRequestRule> = {
+    requestCheckInDirectMessage: true, // The message will not be sent as a direct message
+    requestCheckInOrganizationChannel: false,
+};
